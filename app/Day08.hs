71b3ce85308f0504057c80c0a0bce85a72361023
Luke Moran <luke.moran2004@gmail.com>
2022-12-13 17:56:48 +0000

    Finished Day 8 in Haskell

--- app/Day08.hs
+++ app/Day08.hs
@@ -1,10 +1,26 @@
 module Day08 where
-import Data.Ix
+import Data.List
+import Data.Char
 import Utils
+import Debug.Trace
 
 
+checkLstLR row = reverse  $ go row (-1) [] where
 
-tlst = [[1,2,3], [4,5,6], [7,8,9]]
+    go :: [Int] -> Int -> [Int] -> [Int]
+
+    go [] _ acc = acc
+    go (v:vs) msf acc
+        | v > msf = go vs v (1:acc)
+        | otherwise = go vs msf (0:acc)
+
+merge :: [Int] -> [Int] -> [Int]
+merge = zipWith comp  where
+    comp 0 1 = 1
+    comp 1 0 = 1
+    comp 1 1 = 1
+    comp 0 0 = 0
+
 
 getRow :: [[Int]] -> Int -> [Int]
 getRow lst r = lst !! r
@@ -12,31 +28,58 @@ getRow lst r = lst !! r
 getCol :: [[Int]] -> Int -> [Int]
 getCol lst c = [row!!c | row <- lst]
 
-checkSee :: [[Int]] -> Int -> Int -> Bool
-checkSee lst row col  = flag where
+
+checkLst lst = merge (checkLstLR lst) (reverse $ checkLstLR $ reverse lst)
+
+
+checkRows = map checkLst 
+checkCols =  transpose . map checkLst  . transpose
+
+checkGrid grid = sum [sum $ merge a b | (a,b) <- zip (checkRows grid) (checkCols grid)]
+
+tlst :: [[Int]]
+tlst = [[1,2,3,3,7], 
+        [6,2,4,1,5], 
+        [7,8,9,3,5]]
+
+
+countSee [] val = 0
+countSee (r:rs) val
+    | r < val = 1 + countSee rs val
+    | otherwise = 1
+
+checkSee :: [[Int]] -> Int -> Int -> Int
+checkSee lst row col  = product ls where
     (a,(it:b)) = splitAt col (getRow lst row)
     (c,(_:d)) = splitAt row (getCol lst col)
+    
+    v = lst!!row!!col
+    ls = map (flip countSee v) [reverse a,b,reverse c,d]
+    
 
-    l = [a,b,c,d]
-    flag = any null l || any (< it) (map maximum l)
-
-
-readInt :: Char -> Int
-readInt c = read [c]
 
 day8Part1 = do
     inp <- readFile "inputs/Day8.in"
 
 
 
-    let grid = [map readInt i |i <- lines inp]
-    let s = [1| row <- [0.. (length grid - 1)], col <- [0..(length grid - 1)], checkSee grid row col ]
+    let grid = [map digitToInt i |i <- lines inp]
+    --let s = [1| row <- [0.. (length grid - 1)], col <- [0..(length grid - 1)], checkSee grid row col ]
 
 
-    print $ sum s
+    --print $ checkCols grid
+    print $ checkGrid grid
 
 day8Part2 = do
-    print "undone"
+    inp <- readFile "inputs/Day8.in"
+
+
+
+    let grid = [map digitToInt i |i <- lines inp]
+
+
+    let s = maximum [checkSee grid row col | row <- [0.. (length grid - 1)], col <- [0..(length grid - 1)]]
+    print s
 
 
 day8 = solveDay day8Part1 day8Part2 8
