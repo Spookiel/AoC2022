13fb411619de6025c14b5dba0caf105b4a01704a
Luke Moran <luke.moran2004@gmail.com>
2022-12-14 14:08:49 +0000

    Cleaned up Haskell Day 5 with IntMap

--- app/Day05.hs
+++ app/Day05.hs
@@ -2,12 +2,16 @@ module Day05 where
 import Data.List (groupBy,sort)
 import Data.List.Index (indexed)
 import Data.List.Split (splitOn, chunksOf)
+import Data.IntMap (IntMap, (!), adjust, elems, insertWith)
+import qualified Data.IntMap as IM
 import Data.Maybe
 import Text.Read
+import Debug.Trace
 import Utils
 
 type Stack = String
 type Wharf = [Stack]
+type FastWharf = IntMap Stack
 type Instruction = [Int]
 
 
@@ -15,69 +19,80 @@ parseRow :: String -> [(Int, Char)]
 parseRow  s = filter (\(a,b) -> b /= ' ') (map (\(a,b) -> (a, head b)) (indexed $ chunksOf 4 (tail s)))
 
 
-buildNewStack:: Stack -> (Int, Char) -> Stack
-buildNewStack s (tind, nchar) = nchar:s
+moveOneFast :: FastWharf -> Int -> Int -> FastWharf
+moveOneFast wharf fr to = nwharf2 where
+    
+    tm = head $ wharf ! fr
+    nwharf1 = adjust tail fr wharf
+    nwharf2 = adjust (tm:) to nwharf1
+moveManyFast :: FastWharf -> Int -> Int -> Int -> FastWharf
+moveManyFast swharf n fr to = nw2 where
+    tm = take n $ swharf ! fr
+    nw1 = adjust (drop n) fr swharf
+    nw2 = adjust (tm ++) to nw1
+
+execute9000 :: FastWharf -> [Int] -> FastWharf
+--exceute9000 swharf [0,a,b] = swharf
+execute9000 swharf [n,a,b] 
+    | n > 0 =  execute9000 (moveOneFast swharf a b) [n-1, a, b]
+    | otherwise = swharf
+
+execute9001 :: FastWharf -> [Int] -> FastWharf
+execute9001 swharf [n,a,b] = moveManyFast swharf n a b
 
 
-buildRow :: Wharf -> [(Int, Char)] -> Wharf
-buildRow wharf sframe = [fromMaybe (wharf!!i) (lookup i changed)| i <- [0..(length wharf -1 )]] where
-    changed = [(tind, buildNewStack (wharf!!tind) (tind, nchar))| (tind, nchar) <- sframe]
-
--- Replaces stack at index with the new stack
-replaceStack :: Wharf -> Int -> Stack -> Wharf
-replaceStack wharf tind nstack = [if(i==tind) then nstack else j|(i, j) <- indexed wharf]
-
-buildStacks :: [[(Int, Char)]] -> Wharf
-buildStacks prows = map reverse $ foldl buildRow [[] | i <- [0..9]] prows
 
 
-moveN :: Int -> Stack -> Stack -> (Stack, Stack)
-moveN n s1 s2 = (drop n s1, added ++ s2) where added = take n s1
+buildIMRowSingle :: FastWharf -> (Int, Char) -> FastWharf
+buildIMRowSingle swharf (snum, schar) = insertWith (flip (++)) (snum+1) [schar] swharf
+
+buildCompleteRow :: FastWharf -> [(Int, Char)] -> FastWharf
+buildCompleteRow = foldl buildIMRowSingle
+
 
 parseLine :: String -> [Int]
 parseLine line = mapMaybe readMaybe $ words line
-
-applyInst1 :: Wharf -> Instruction -> Wharf
-applyInst1 wharf [0 ,b,c] = wharf
-applyInst1 wharf [n,b,c] = applyInst1 nwharf2 [n-1,b,c] where
-    ostack1 = wharf!!(b-1)
-    ostack2 = wharf!!(c-1)
-    (nstack1, nstack2) = moveN 1 ostack1 ostack2
-    nwharf1 = replaceStack wharf (b-1) nstack1
-    nwharf2 = replaceStack nwharf1 (c-1) nstack2
-
-
-applyInst2 :: Wharf -> Instruction -> Wharf
-applyInst2 wharf [0 ,b,c] = wharf
-applyInst2 wharf [n,b,c] = applyInst1 nwharf2 [0,b,c] where
-    ostack1 = wharf!!(b-1)
-    ostack2 = wharf!!(c-1)
-    (nstack1, nstack2) = moveN n ostack1 ostack2
-    nwharf1 = replaceStack wharf (b-1) nstack1
-    nwharf2 = replaceStack nwharf1 (c-1) nstack2
 
 
 parse5 = do
     inp <- readFile "inputs/Day5.in"
     let [a,b] = splitOn "\n\n" inp
-    let stacks = buildStacks (init (map parseRow (lines a)))
+    let sinfo = init (map parseRow (lines a))
+    let stacks = foldl buildCompleteRow IM.empty sinfo
     let insts = map parseLine $ lines b
     return (stacks, insts)
+
+getFirsts :: [String] -> String
+getFirsts = mapMaybe safeHead 
+
 
 day5Part1 = do
     inp <- readFile "inputs/Day5.in"
     (stacks, insts) <- parse5
-    let finished = foldl applyInst1 stacks insts
-    let answer = mapMaybe safeHead finished
-    print answer
+    -- let finished = foldl applyInst1 stacks insts
+    -- let answer = mapMaybe safeHead finished
+    -- print answer
+
+    --let fstacks = IM.fromList $ zip [1..10] stacks
+    --print fstacks
+    let answer = foldl execute9000 stacks insts
+
+
+    print $ getFirsts $  elems answer
 
 -- let stacks = ["ABC", "", ""]
 -- let insts1 = [[3,1,3]]
 
 day5Part2 = do
     (stacks, insts) <- parse5
-    let finished = foldl applyInst2 stacks insts
-    let answer = mapMaybe safeHead finished
-    print answer
+    -- let finished = foldl applyInst2 stacks insts
+    -- let answer = mapMaybe safeHead finished
+    -- print answer
 
+
+    --let fstacks = IM.fromList $ zip [1..10] stacks
+
+    let answer = foldl execute9001 stacks insts
+
+    print $ getFirsts $ elems answer
 day5 = solveDay day5Part1 day5Part2 5
